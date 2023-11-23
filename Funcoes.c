edfdf061293e256ae74950c3a01f7aaf75a32022
Pedro Santos <pedrohenriqueph004@gmail.com>
2023-11-23 15:25:52 -0300

    Editando a função 1 e o menu

--- Funcoes.c
+++ Funcoes.c
@@ -22,35 +22,41 @@ void inserir_nova_tarefa(Lista l) {
     int status;
    
 
-    printf("Digite o nome da tarefa em até 50 caracteres:");
+    printf("\nDigite o nome da tarefa em ate 50 caracteres:");
     scanf(" %49[^\n]", nome);
     //Pega a tarefa l e dentro dela a posicao que ela está e altera só o nome dessa tarefa, dentro da lista de tarefas
     strcpy(l.tarefas[l.posicaoAtual].Nome , nome);
 
-    printf ("Digite a dificuldade da tarefa, sendo 1 facil, 2 medio e 3 para uma tarefa dificil:");
+    printf ("Digite a dificuldade da tarefa, sendo:\n");
+    printf ("1 - Facil\n");
+    printf ("2 - Media\n");
+    printf ("3 - Dificil\n");
+     
     scanf("%d", &dificuldade);
     if (dificuldade < 1 || dificuldade > 3)
     {
         printf("Dificuldade invalida!\n");
         return;
     }
+    //isso daqui é só um if que se dificuldade for igual a 1 aparece facul e tarara
     strcpy(l.tarefas[l.posicaoAtual].Dificuldade, dificuldade == 1 ? "facil" : dificuldade == 2 ? "Medio" : "Dificil");
 
-    printf ("Digite os status da tarefa, use 1 para tarefa não concluída e 2 para tarefa concluida:");
+    printf ("Digite os status da tarefa, use:\n");
+    printf ("1 - Tarefa nao concluida \n");
+    printf ("2 - Tarefa concluida\n");
     scanf("%d", &status);
     if (status < 1 || status > 2)
     {
         printf("Status invalida!\n");
         return;
     }
-    strcpy(l.tarefas[l.posicaoAtual].Status, status == 1 ? "Não concluida" : "Concluida");
+    strcpy(l.tarefas[l.posicaoAtual].Status, status == 1 ? "Nao concluida" : "Concluida");
 
-    printf("tarefa inserida com sucesso!\n");
-    printf("Nome :%s\n", l.tarefas[l.posicaoAtual].Nome);
-    printf("Dificuldade :%s\n", l.tarefas[l.posicaoAtual].Dificuldade);
-    printf("Status :%s\n", l.tarefas[l.posicaoAtual].Status);
+    printf("\ntarefa inserida com sucesso!\n");
+    printf("\nNome: %s\n", l.tarefas[l.posicaoAtual].Nome);
+    printf("Dificuldade: %s\n", l.tarefas[l.posicaoAtual].Dificuldade);
+    printf("Status: %s\n", l.tarefas[l.posicaoAtual].Status);
     
-
 }
 
 void mostrar_tarefas_cadastradas() {
--- Menu.c
+++ Menu.c
@@ -25,22 +25,23 @@ int main() {
     l.tarefas = (Tarefa*)calloc(10, sizeof(Tarefa)); 
 
     //Da posição 0 a lista para a primeira ser a 0
-    l.posicaoAtual = 0;
+    l.posicaoAtual = 0;    
 
     do {
-        printf("Escolha uma opcao:\n");
-        printf("1. Inserir nova tarefa\n");
+        printf("\nO que deseja fazer?\n");
+        printf("\n1. Inserir nova tarefa\n");
         printf("2. Mostrar tarefas cadastradas\n");
         printf("3. Buscar por tarefa\n");
         printf("4. Editar informacoes de uma tarefa\n");
         printf("5. Remover tarefa\n");
         printf("6. Marcar conclusao de uma tarefa especifica\n");
-        printf("7. Salvar lista de tarefas em um arquivo, incluindo se cada uma foi finalizada ou nao\n");
+        printf("7. Salvar lista de tarefas em um arquivo\n");
+        printf("8. Sair\n");
 
         scanf("%d", &opcao);
 
         if (opcao < 1 || opcao > 7) {
-            printf("Opcao invalida. Digite uma opcao valida.\n");
+            printf("\nOpcao invalida. Digite uma opcao valida.\n");
         } 
 
         else {
@@ -68,6 +69,9 @@ int main() {
             break;
         case 7:
             salvar_lista_de_tarefas_em_um_arquivo();
+            break;
+
+        case 8:
             break;
         }
     }} while (opcao != 8);
